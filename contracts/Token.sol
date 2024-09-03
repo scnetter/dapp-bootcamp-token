62ea34d72fbcb8ae0fa14eb9426b469a22dddb68
Kelly Netterville <15361880+scnetter@users.noreply.github.com>
2024-09-03 15:13:52 -0500

    Update package.json and complete first smoke test deployed token.

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,8 +1,9 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+//SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.4;
 
-
+// Allows us to do console logging on the contracts under dev
+import "hardhat/console.sol";
 
 contract Token {
-	string public name = "Lima Coin";
+	string public name = "LIMA Coin";
 }
---
+++ scripts/1_deploy.js
@@ -0,0 +1,16 @@
+async function main() {
+	const Token = await ethers.getContractFactory("Token");
+	const token = await Token.deploy();
+	await token.deployed();
+
+	// No need to call token.deployed() explicitly
+	console.log(`Token deployed to: ${token.address}`);
+}
+
+
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
