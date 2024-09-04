7315430a43ef23c27eb59dc3d909952b7c813c99
Kelly Netterville <15361880+scnetter@users.noreply.github.com>
2024-09-04 17:55:16 -0500

    Create constructor for token

--- test/Token.js
+++ test/Token.js
@@ -1,13 +1,18 @@
 const { ethers } = require('hardhat');
 const { expect } = require('chai');
 
+// Helper function to convert tokens to value x 10^ether_decimals (18)
+const tokens = (n) => {
+	return ethers.utils.parseUnits(n.toString(), 'ether');
+}
+
 describe('Token', () => {
 	let token;
 
 	beforeEach( async () => {
 		// Test setup - deploy and fetch token
 		const Token = await ethers.getContractFactory('Token');
-		token = await Token.deploy();
+		token = await Token.deploy("Lima Coin", "LIMA", 1000000);
 	});
 
 	// Tests go here
@@ -19,5 +24,14 @@ describe('Token', () => {
 	it('has correct symbol', async () => {
 		// Check symbol is correct
 		expect(await token.symbol()).to.equal('LIMA');
+	});
+	it('has correct decimals', async () => {
+		// Check symbol is correct
+		expect(await token.decimals()).to.equal('18');
+	});
+		it('has correct totalSupply', async () => {
+	// Check symbol is correct
+		
+		expect(await token.totalSupply()).to.equal(tokens(1000000));
 	});
 });
